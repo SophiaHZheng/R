7c302aa9ebbe56e0f561648579dbcc3ddb517798
SophiaHZheng <hanyue.zheng@utexas.edu>
2016-11-06 15:59:23 -0600

    Update 3.1 Loop and Flow Control(2).R

--- 3.1 Loop and Flow Control(2).R
+++ 3.1 Loop and Flow Control(2).R
@@ -9,6 +9,33 @@ for (i in 1:k2){
   m[i] <- colMeans(df)[i]
 }
 mean1 <- mean(m)
-
-
 # also can use:     m[i] <- mean(df[,i])
+
+
+
+#Write a loop that loops over the columns of x, and for each column stores 
+#the minimum, median, mean and maximum in the corresponding column of my.summary.
+
+k<-1000
+r<-100
+set.seed(5556)
+my.data <- as.data.frame(matrix(rnorm(r*k), nrow=r))
+
+my.summary <- matrix(nrow=4,ncol=k)
+
+for (i in 1:k){
+  my.summary[1,i] <- min(my.data[,i])
+  my.summary[2,i] <- median(my.data[,i])
+  my.summary[3,i] <- mean(my.data[,i])
+  my.summary[4,i] <- max(my.data[,i])
+}
+
+#Write a function my.function(), which takes a vector argument, and returns a length-4 vector of 
+#minimum, median, mean and maximum of the input. Then recalculate my.summary using sapply().
+
+my.function <- function(x){
+  v <- c(min(x), median(x), mean(x), max(x))
+}
+sapply(my.data, my.function)
+
+##TWO WAYS OF CALCULATION: sapply will be much faster than loop!!!
